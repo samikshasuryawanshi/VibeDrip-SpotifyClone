edf008499aac94d55d95cdebee6a6ada2c53f98e
samiksha suryawanshi <suryawanshisamiksha506@gmail.com>
2025-10-24 12:40:26 +0530

    feat: update dependencies and add button component with utility functions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
+import { Button } from "./components/ui/button"
 
 function App() {
   return (
     <>
       <h1 className="text-4xl text-red-500">hello</h1>
+      <Button variant={"outline"}>Click me</Button>
     </>
   )
 }
