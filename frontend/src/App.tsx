04f531054acb76873be1961b16203245b9b32949
samiksha suryawanshi <suryawanshisamiksha506@gmail.com>
2025-10-27 15:20:02 +0530

    Initialize backend project with package.json and essential dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import {Button} from"./components/ui/button";
+
 
 export default function App() {
   return (
     <header>
       <SignedOut>
-        <SignInButton />
+        <SignInButton>
+          <Button>Sign In</Button>
+        </SignInButton>
       </SignedOut>
       <SignedIn>
         <UserButton />
