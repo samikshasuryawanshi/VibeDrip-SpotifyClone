04ef257cd37a9091cc3a4228c38f6f6948bc4204
samiksha suryawanshi <suryawanshisamiksha506@gmail.com>
2025-10-29 17:13:23 +0530

    feat: implement user retrieval functionality; add getAllUsers controller and protect route middleware

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,9 @@
 import {Router} from 'express';
+import { protectRoute } from '../middleware/authMiddleware.js';
+import { getAllUsers } from '../controller/userController.js';
 const router = Router();
 
 // Example route
-router.get('/', (req, res) => {
-    res.send('User route is working');
-});
+router.get('/',protectRoute, getAllUsers);
 
 export default router;
