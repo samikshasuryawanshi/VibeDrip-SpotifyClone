110e9c4fc27834fef679e77dfdf57a853264d126
samiksha suryawanshi <suryawanshisamiksha506@gmail.com>
2025-10-29 16:13:01 +0530

    feat: implement song retrieval functionality; add getAllSongs, getFeaturedSongs, getMadeForYouSongs, and getTrendingSongs controllers

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -4,8 +4,6 @@ export const protectRoute = async (req, res, next) => {
     if(!req.auth.userId){
         return res.status(401).json({message:"Unauthorized - You must be loggedIn"});
     }
-
-
     next();
 };
 
--- backend/src/routes/songs.route.js
+++ backend/src/routes/songs.route.js
@@ -1,9 +1,13 @@
 import { Router} from "express";
+import { getAllSongs, getFeaturedSongs, getMadeForYouSongs, getTrendingSongs } from "../controller/songController.js";
+import { protectRoute, requireAdmin} from "../middleware/authMiddleware.js";
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.send("song Route Working");
-});
+router.get("/",protectRoute,requireAdmin, getAllSongs);
+router.get("/featured",getFeaturedSongs);
+router.get("/made-for-you",getMadeForYouSongs);
+router.get("/trending",getTrendingSongs);
+
 
 
 export default router;
