de4657851f3d1e69cf203502b9aab73f690f50e7
samiksha suryawanshi <suryawanshisamiksha506@gmail.com>
2025-11-08 14:46:18 +0530

    isMobile setup

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -5,9 +5,23 @@ import FriendsActivity from "./components/FriendsActivity";
 import { ErrorBoundaryWrapper } from "@/components/ErrorBoundaryWrapper";
 import AudioPlayer from "./components/AudioPlayer";
 import PlaybackControls from "./components/PlaybackControls";
+import { useEffect, useState } from "react";
 
 const MainLayout = () => {
-  const isMobile = false;
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    }
+
+    checkMobile();
+    window.addEventListener('resize', checkMobile);
+
+    return () => {
+      window.removeEventListener('resize', checkMobile);
+    }
+  })
 
   return (
     <div className="h-screen bg-black text-white flex flex-col">
@@ -37,20 +51,25 @@ const MainLayout = () => {
           <Outlet />
         </ResizablePanel>
 
-        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+        
+        {!isMobile && (
+          <>
+            <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
 
-        {/* Right Sidebar (Friends Activity) */}
-        <ResizablePanel
-          defaultSize={20}
-          minSize={0}
-          maxSize={25}
-          collapsedSize={0}
-          className="bg-[linear-gradient(to_right,rgb(17,17,17),rgb(22,22,22),rgb(17,17,17))] rounded-md p-2"
-        >
-          <ErrorBoundaryWrapper>
-            <FriendsActivity />
-          </ErrorBoundaryWrapper>
-        </ResizablePanel>
+            {/* Right Sidebar (Friends Activity) */}
+            <ResizablePanel
+              defaultSize={20}
+              minSize={0}
+              maxSize={25}
+              collapsedSize={0}
+              className="bg-[linear-gradient(to_right,rgb(17,17,17),rgb(22,22,22),rgb(17,17,17))] rounded-md p-2"
+            >
+              <ErrorBoundaryWrapper>
+                <FriendsActivity />
+              </ErrorBoundaryWrapper>
+            </ResizablePanel>
+          </>
+        )}
       </ResizablePanelGroup>
 
 
